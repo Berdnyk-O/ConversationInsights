bd6fbef615a0b30de2b6bb92abd0ef968a50c463
Berdnyk-O <berdnyk.oleksandr.v@gmail.com>
2024-10-02 16:31:50 +0300

    create category endpoints

---
+++ ConversationInsights.API/Endpoints/CategoryEndpoints.cs
@@ -0,0 +1,28 @@
+﻿namespace ConversationInsights.API.Endpoints
+{
+    public static class CategoryEndpoints
+    {
+        public static void MapCategoryEndpoints(this IEndpointRouteBuilder app)
+        {
+            app.MapGet("/category", async () =>
+            {
+                return Results.NotFound();
+            });
+
+            app.MapPost("/category", async () =>
+            {
+                return Results.NotFound();
+            });
+
+            app.MapPut("/category/{category_id}", async (string category_id) =>
+            {
+                return Results.NotFound();
+            });
+
+            app.MapDelete("/category/{category_id}", async (string category_id) =>
+            {
+                return Results.NotFound();
+            });
+        }
+    }
+}
