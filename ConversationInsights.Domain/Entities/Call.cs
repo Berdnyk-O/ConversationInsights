61dbb38e5617f0b613956b55902a711ee4eb789c
Berdnyk-O <berdnyk.oleksandr.v@gmail.com>
2024-10-02 16:56:42 +0300

    add call entity and category entity

---
+++ ConversationInsights.Domain/Entities/Call.cs
@@ -0,0 +1,20 @@
+﻿using ConversationInsights.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversationInsights.Domain.Entities
+{
+    public class Call
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Location { get; set; } = null!;
+        public EmotionalTone EmotionalTone { get; set; }
+        public string Text { get; set; } = null!;
+        public List<Category> Categories { get; } = [];
+        public List<CallCategory> CallCategories { get; } = [];
+    }
+}
---
+++ ConversationInsights.Domain/Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace ConversationInsights.Domain.Entities
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string[] Points { get; set; } = null!;
+        public List<Call> Calls { get; } = [];
+        public List<CallCategory> CallCategories { get; } = [];
+       
+    }
+}
